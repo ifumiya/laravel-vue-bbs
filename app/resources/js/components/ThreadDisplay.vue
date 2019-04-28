356e8efb1a72cc73a5d6a638a9e73218b62471de
FODK <inukaifumiya+dev@gmail.com>
2019-04-28 21:45:48 +0900

    feat: スレッドコンポーネントの追加

---
+++ app/resources/js/components/ThreadDisplay.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div>
+            <h2> Title </h2>
+            <p>
+                message
+            </p>
+        </div>
+        <div>
+            <input type="text">
+            <input type="text">
+            <input type="submit">
+        </div>
+    </div>
+</template>
---
+++ app/resources/js/components/ThreadForm.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <div class="root">
+            <div class="name-row labeled-input">
+                <label>Name:</label>
+                <input type="text" class="name-input">
+            </div>
+            <div class="title-row labeled-input">
+                <label>Title:</label>
+                <input type="text" class="title-input">
+            </div>
+            <textarea class="message-input"></textarea>
+            <input type="submit" class="submit-button">
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+.root
+{
+    border: 1px solid gray;
+    padding: 4px;
+}
+
+.labeled-input
+{
+    display: flex;
+}
+
+.labeled-input label
+{
+    width: 6em;
+    text-align: right;
+}
+
+.labeled-input input
+{
+    flex-grow: 1;
+}
+.message-input
+{
+    width: 100%;
+    height: 12em;
+}
+
+
+</style>
+
