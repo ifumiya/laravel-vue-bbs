597fa4f7a5e95f32a7310ba6e542243db4eb657b
FODK <inukaifumiya+dev@gmail.com>
2019-04-28 21:45:22 +0900

    feat: ルートコンポーネントの追加

---
+++ app/resources/js/components/Root.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="root">
+        <h1> L.V. BSS</h1>
+        <thread-form/>
+        <thread-display/>
+        <thread-display/>
+        <thread-display/>
+    </div>
+</template>
+
+<style scoped>
+
+.root
+{
+    margin: 0px 20%;
+}
+
+</style>
