7bcdeccdd209cd47b82f3d8ce2852ded2719b62e
FODK <inukaifumiya+dev@gmail.com>
2019-04-30 16:04:17 +0900

    feat: threadsテーブルマイグレーション追加

---
+++ app/database/migrations/2019_04_30_054731_create_threads_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateThreadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('threads', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('name', 100)
+                ->comment('投稿者');
+            $table->string('title', 500)
+                ->comment('スレッドタイトル');
+            $table->text('message')
+                ->comment('内容');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('threads');
+    }
+}
